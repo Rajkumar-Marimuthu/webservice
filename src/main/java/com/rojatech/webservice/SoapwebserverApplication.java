615cb68ba618c596059b5b652b6fd4a215cbfed4
“Rajkumar-Marimuthu” <rajkumartechgy@gmail.com>
2024-01-20 05:56:36 +0000

    initialize web service project

---
+++ src/main/java/com/rojatech/webservice/SoapwebserverApplication.java
@@ -0,0 +1,13 @@
+package com.rojatech.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapwebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapwebserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rojatech/webservice/SoapwebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojatech.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapwebserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
